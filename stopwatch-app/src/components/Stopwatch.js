fcb33a1dc540dcf645ffa0e94551233d3973441b
TamilselvanChandrakumar <tamilbin001@gmail.com>
2023-12-08 12:18:40 +0530

    first commit

--- stopwatch-app/src/components/Stopwatch.js
+++ stopwatch-app/src/components/Stopwatch.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Stopwatch = () => {
-  return <div>Stopwatch</div>;
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+  useEffect(() => {
+    let interval = null;
+    if (start) {
+      interval = setInterval(() => {
+        setTime((prevtime) => prevtime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [start]);
+  return (
+    <>
+      <h1>Stopwatch</h1>
+      <p>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 1000)).slice(-2)}</span>
+      </p>
+      <div>
+        <button onClick={() => setStart(true)}>start</button>
+        <button onClick={() => setStart(false)}>stop</button>
+        <button
+          onClick={() => {
+            setTime(0);
+            setStart(false);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Stopwatch;
